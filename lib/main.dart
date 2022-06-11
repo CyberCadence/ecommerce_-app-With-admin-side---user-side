9d269cb96f44503cd385dfe2d310f0fcab47d0b5
CyberCadence <86359099+CyberCadence@users.noreply.github.com>
2022-06-11 10:50:10 +0530

    cloud functions enabled js

--- lib/main.dart
+++ lib/main.dart
@@ -9,27 +9,30 @@ import 'package:ecomm/firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  // ignore: prefer_const_constructors
-  runApp(ProviderScope(child:  MyApp()));
-} 
+  Stripe.publishableKey = 'pk_test_51L83VXSCFOe57vGMDOEMyVlq7WXcOjozYZu3o2xh2zc0XNvkqBTFXFxetDGChOCT66LZsi1oTnNIkiRlvqVks44S00zBnzrwHK';
+  runApp(const ProviderScope(child: MyApp()));
+}
 
-class MyApp extends ConsumerWidget{
+class MyApp extends ConsumerWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return MaterialApp(debugShowCheckedModeBanner: false,
-      theme: ThemeData(colorScheme:ColorScheme.fromSeed(seedColor: Colors.orange,primary: Colors.orange)) ,
-      home: AuthWidget(adminSignedInBuider: (context) => AdminHome(),
-        signinBuilder: (context) =>   const  UserHomePage(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: Colors.orange, primary: Colors.orange)),
+      home: AuthWidget(
+        adminSignedInBuider: (context) => AdminHome(),
+        signinBuilder: (context) => const UserHomePage(),
         noSigninBuilder: (context) {
-         return const Scaffold(
-            body: SignInPage()
-          );
+          return const Scaffold(body: SignInPage());
         },
       ),
     );
